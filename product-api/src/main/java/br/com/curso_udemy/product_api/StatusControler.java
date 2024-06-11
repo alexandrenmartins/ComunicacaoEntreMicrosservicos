a3f44bc774437fa6b8ae861b90be6a7a90e9a6f8
Alexandre Martins <alexandre.martins@aspec.com.br>
2024-06-11 11:04:45 -0300

    Status Controler

---
+++ product-api/src/main/java/br/com/curso_udemy/product_api/StatusControler.java
@@ -0,0 +1,32 @@
+package br.com.curso_udemy.product_api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api")
+public class StatusControler {
+
+    @GetMapping("teste")
+    public String requestMethodName(@RequestParam String nome) {
+        return new String("Seja bem-vindo " + nome);
+    }
+
+    @GetMapping("status")
+    public ResponseEntity<HashMap<String, Object>> getApiStatus() {
+        var response = new HashMap<String, Object>();
+
+        response.put("service", "Product-API");
+        response.put("status", "up");
+        response.put("httpStatus", HttpStatus.OK.value());
+
+        return ResponseEntity.ok(response);
+    }
+
+}
